339edf19340db1cbf79ad94c2c3ea34ef79875fe
ermias <ermiasgashaw7881@gmail.com>
2022-05-17 00:30:52 -0700

    task 0

--- mod_character.c
+++ mod_character.c
@@ -11,6 +11,10 @@ int (*mod_character_s(const char *next, int dino))(va_list)
 	spc_t options[] = {
 		{"s", print_string},
 		{"c", print_char},
+		{"i", print_number},
+		{"d", print_number},
+		{"u", print_unsigned},
+		{"r", print_reverse},
 		{NULL, NULL}
 	};
 
